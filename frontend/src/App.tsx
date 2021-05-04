b92c55597bb468acf6f2f7112074330c8d53f233
Thiago <thiagolima1011@gmail.com>
2021-05-04 19:10:42 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+import DataTable from "components/dataTable";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá mundo</h1>
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,14 @@
+import ImgDsDark from 'assets/img/ds-dark.svg';
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/tlima1011" target="_blank" rel="noreferrer">Thiago Moura</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
